0a8c30aaf8684128de1a1ea12e0639bdc3bf5f4c
Stas Ukolov <ukoloff@gmail.com>
2017-01-01 19:50:17 +0500

    Remove all files

--- lib/without/rails.rb
+++
@@ -1,8 +0,0 @@
-require "without/rails/version"
-
-module Without
-  module Rails
-    class Engine < ::Rails::Engine
-    end
-  end
-end
--- lib/without/rails/version.rb
+++
@@ -1,5 +0,0 @@
-module Without
-  module Rails
-    VERSION = "1.2.0"
-  end
-end
